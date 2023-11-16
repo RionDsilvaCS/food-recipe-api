745949a79ca95347fdca525abeca3fe0cb38818d
Rion Dsilva <118284932+RionDsilvaCS@users.noreply.github.com>
2023-11-16 19:09:02 +0530

    add update and delete routes

--- routes/recipe/recipes.js
+++ routes/recipe/recipes.js
@@ -3,25 +3,29 @@ const Recipe = require('../../models/Recipe');
 const jwt = require("jsonwebtoken");
 const verify = require("../auth/authVerify")
 
-router.get('/get-recipe/:id',verify, async (req, res) => {
+router.get('/get-recipe/:name',verify, async (req, res) => {
 
-    const recipe = await User.findOne({_id: req.body.id});
-    if (!recipe){
+    const recipe = {recipe_name: req.params.name}
+
+    const recipe_info = await Recipe.findOne(recipe).exec();
+    if (!recipe_info){
         res.status(400).send("Recipe dosen't exists");
         return;
     }
 
-    res.status(200).send(recipe)
+    res.status(200).send(recipe_info)
 });
 
 
 router.get('/get-recipe-list',verify, async (req, res) => {
+
     try{
         const recipe_list = await Recipe.find().exec();
         res.send(recipe_list)
     } catch (error){
         res.status(500).send(error);
     }
+
 });
 
 
@@ -40,19 +44,41 @@ router.post('/create-recipe',verify, async (req, res) => {
     }catch(error){
         res.status(500).send(error);
     }
+    
 });
 
 
 router.put('/update-recipe',verify, async (req, res) => {
 
-    // update a recipe
+    const recipe = new Recipe({
+        recipe_name: req.body.recipe_name,
+        calorie: req.body.calorie,
+        image: req.body.image,
+        items: req.body.items,
+    });
+
+    const filter = {recipe_name: req.body.recipe_name}
+
+    try{
+        const updateRecipe = await Recipe.findOneAndUpdate(filter, recipe);
+        res.status(200).send(updateRecipe);
+    }catch(error){
+        res.status(500).send(error);
+    }
 
 });
 
 
 router.delete('/delete-recipe',verify, async (req, res) => {
 
-    // delete a recipe
+    const recipe = {recipe_name: req.body.recipe_name}
+
+    try{
+        const deleteRecipe = await Recipe.deleteOne(recipe);
+        res.status(200).send(deleteRecipe);
+    }catch(error){
+        res.status(500).send(error);
+    }
 
 });
 
